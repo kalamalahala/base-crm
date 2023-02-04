741178e4d5a7e1d3feccabe5d42dba62a8eaabbe
kalamalahala <solo.driver.bob@gmail.com>
2023-02-04 00:41:25 -0500

    version update before next session

--- BaseCRM.php
+++ BaseCRM.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 0.0.1 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'BaseCRM_VERSION', '0.0.1' );
+define( 'BaseCRM_VERSION', '0.0.5' );							// current version 0.0.5
 define( 'BaseCRM_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'BaseCRM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
