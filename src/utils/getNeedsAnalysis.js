6acd08bd9cb3be0858d6eed65d2007988f5ef417
kalamalahala <solo.driver.bob@gmail.com>
2023-07-13 17:28:54 -0400

    minor text changes and UI updates
    Signed-off-by: kalamalahala <solo.driver.bob@gmail.com>

--- src/utils/getNeedsAnalysis.js
+++ src/utils/getNeedsAnalysis.js
@@ -3,12 +3,13 @@ import {pendButton} from "./ajaxTestingZone";
 const $ = jQuery;
 export function getNeedsAnalysis(e) {
     e.preventDefault();
-    console.log('clicked ' + e.target);
-    console.log('data: ', e.target.dataset);
+
     let leadId = $(e.target).data('id')
     let formId = base_crm.na_form_id;
     let gkId = base_crm.gview_id;
-    console.log(`leadId: ${leadId}, formId: ${formId}, gkId: ${gkId}`);
+    let name = $(e.target).data('first-name') + ' ' + $(e.target).data('last-name');
+
+    $('#modal-client-info-name').html(name);
 
     // query gravityforms rest api for most recent entry where field 18 (lead_id) matches leadId
     let gfEndpoint = 'gf/v2/entries?form_ids[0]=' + formId + 'search={"field_filters":[{"key":"18","value":"' + leadId + '"}]}';
@@ -20,7 +21,9 @@ export function getNeedsAnalysis(e) {
         beforeSend: (xhr) => {
             xhr.setRequestHeader('X-WP-Nonce', wpApiSettings.nonce)
             console.log('URL: ' + wpApiSettings.root + gfEndpoint);
-            $('#modal-client-info-body').html('Fetching Needs Analysis ...');
+            $('#modal-client-info-body').html(`
+                <div class="spinner-border text-primary" role="status"></div>Searching for ${name}'s Needs Analysis Entry...
+            `);
             $('#modal-client-info').modal('show');
         },
         success: (response) => {
@@ -29,17 +32,19 @@ export function getNeedsAnalysis(e) {
                 $('#modal-client-info-body').html('No Needs Analysis Found');
                 return;
             }
+            let count = response.entries.length;
             let entryId = response.entries[0].id;
             console.log('success, entry id: ' + entryId);
 
-            let gravityKitEndpoint = `gravityview/v1/views/${gkId}/entries/${entryId}?gv_response_format=html`;
+            let gravityKitEndpoint = `gravityview/v1/views/${gkId}/entries/${entryId}?html`;
             $.ajax({
                 url: wpApiSettings.root + gravityKitEndpoint,
                 method: 'GET',
                 beforeSend: (xhr) => {
                     xhr.setRequestHeader('X-WP-Nonce', wpApiSettings.nonce);
-                    console.log('GET URL: ' + wpApiSettings.root + gravityKitEndpoint);
-                    $('#modal-client-info-body').html('Fetching Needs Analysis Entry ...');
+                    $('#modal-client-info-body').html(`
+                    <div class="spinner-border text-primary" role="status"></div>Found ${count} Needs Analysis Entries for ${name}. Fetching most recent entry...
+                    `);
                 },
                 success: (response) => {
                     console.log(response);
